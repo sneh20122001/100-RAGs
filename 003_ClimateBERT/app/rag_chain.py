f344c880f69c7eb1386c7d785519b265b8f47ddd
Sneh Patel <95904935+sneh20122001@users.noreply.github.com>
2025-07-07 23:02:33 +0530

    Add files via upload

---
+++ 003_ClimateBERT/app/rag_chain.py
@@ -0,0 +1,49 @@
+from datasets import load_dataset
+from langchain.schema import Document
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import Chroma
+from langchain_google_genai import GoogleGenerativeAIEmbeddings,ChatGoogleGenerativeAI
+from app.config import GOOGLE_API_KEY
+from langchain import hub
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough 
+
+dataset = load_dataset("climatebert/climate_detection")
+
+# Extract texts from the 'text' column
+texts = dataset["train"]["text"]
+
+# Convert texts to Document objects
+documents = [Document(page_content=text) for text in texts]
+
+
+text_splitter = RecursiveCharacterTextSplitter(chunk_size = 2000,
+                                               chunk_overlap = 200)
+splits = text_splitter.split_documents(documents)
+
+
+# Create vector store
+embeddings = GoogleGenerativeAIEmbeddings(model="models/text-embedding-004", google_api_key=GOOGLE_API_KEY)
+vectorstore = Chroma.from_documents(documents=splits, embedding=embeddings)
+retriever = vectorstore.as_retriever()
+
+# Load RAG prompt
+prompt = hub.pull("rlm/rag-prompt")
+
+# LLM model
+llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash", temperature=0, google_api_key=GOOGLE_API_KEY)
+
+# Formatting retrieved docs
+def format_docs(docs):
+    return "\n".join(doc.page_content for doc in docs)
+
+# Create RAG chain
+rag_chain = (
+    {"context": retriever | format_docs, "question": RunnablePassthrough()}
+    | prompt
+    | llm
+    | StrOutputParser()
+)
+
+def ask_question(question):
+    return rag_chain.invoke(question)
